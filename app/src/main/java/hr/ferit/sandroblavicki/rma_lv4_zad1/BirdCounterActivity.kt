d799af59b7f314f5261ae6432b985a070b0569ed
Sandro Blavicki <sandro.blavicki@gmail.com>
2021-05-09 15:32:58 +0200

    First commit

---
+++ app/src/main/java/hr/ferit/sandroblavicki/rma_lv4_zad1/BirdCounterActivity.kt
@@ -0,0 +1,11 @@
+package hr.ferit.sandroblavicki.rma_lv4_zad1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BirdCounterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bird_counter)
+    }
+}
